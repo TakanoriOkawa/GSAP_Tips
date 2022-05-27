5c948d57d85ddf9fc9ac8d99150753b249ba1cac
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-28 00:10:18 +0900

    fix hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-// Before: hello.js
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
-
 // After: hello.ts
-import {NextApiRequest, NextApiResponse} from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  res.status(200).json({ name: 'John Doe' });
+};
